26a3f8ca8064005b775aa5cc0c20b9872d1369c8
Bilal raza <attari1235@gmail.com>
2024-09-15 06:54:52 +0500

    small updates

--- Reactjs/ecommerce/src/components/Footer.jsx
+++ Reactjs/ecommerce/src/components/Footer.jsx
@@ -12,10 +12,10 @@ const Footer = () => {
         <h1>All Rights Reserved</h1>
 
         <div className="flex gap-3 items-center">
-         <a href="" target="_blank"><FacebookOutlined className="text-gray-700 hover:text-blue-600" style={{ fontSize: 35  }} /></a> 
-         <a href="" target="_blank"><InstagramOutlined className="text-gray-700 hover:text-blue-600" style={{ fontSize: 35  }} /></a> 
-         <a href="" target="_blank"><GithubOutlined className="text-gray-700 hover:text-blue-600" style={{ fontSize: 35  }} /></a> 
-         <a href="" target="_blank"><YoutubeOutlined className="text-gray-700 hover:text-blue-600" style={{ fontSize: 35  }} /></a> 
+         <a href="" target="_blank"><FacebookOutlined className="text-gray-700 hover:text-blue-600" style={{ fontSize: 30  }} /></a> 
+         <a href="" target="_blank"><InstagramOutlined className="text-gray-700 hover:text-blue-600" style={{ fontSize: 30  }} /></a> 
+         <a href="" target="_blank"><GithubOutlined className="text-gray-700 hover:text-blue-600" style={{ fontSize: 30  }} /></a> 
+         <a href="" target="_blank"><YoutubeOutlined className="text-gray-700 hover:text-blue-600" style={{ fontSize: 30  }} /></a> 
         </div>
       </div>
     </footer>
